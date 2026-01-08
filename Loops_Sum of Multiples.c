ee7e4068fd542b6da75ddd7b7526f585eaca2942
Sam Gandhi <gandhisam2007@gmail.com>
2026-01-09 00:00:48 +0530

    HACKERRANK DAY-31

---
+++ Loops_Sum of Multiples.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+int n, i; 
+int sum = 0;
+
+scanf("%d", &n);
+
+  for (i = 1; i <= n; i++) {
+    if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+  sum = sum + i;
+}
+}
+  printf("%d", sum);
+return 0;
+}
